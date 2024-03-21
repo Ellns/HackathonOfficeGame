f26a3521cacad7aefceef212de877ed649dae2d6
ChikinNoodleSupp <73000181+ChikinNoodleSupp@users.noreply.github.com>
2024-03-21 15:50:35 +0100

    stuff

--- Assets/Albin/Scripts/Spawner.cs
+++ Assets/Albin/Scripts/Spawner.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject prefab;
+    public float spawnRate = 1f;
+
+    private void OnEnable()
     {
-        
+        InvokeRepeating(nameof(Spawn), spawnRate, spawnRate);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        CancelInvoke(nameof(Spawn));
+    }
+
+    private void Spawn()
+    {
+        GameObject pipes = Instantiate(prefab, transform.position, Quaternion.identity);
     }
 }
