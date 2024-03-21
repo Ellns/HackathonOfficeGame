46fa8d7f6acb0ed8a40484ec3269bcbf7f6860d7
ChikinNoodleSupp <73000181+ChikinNoodleSupp@users.noreply.github.com>
2024-03-21 11:47:16 +0100

    work work

--- Assets/Albin/Scripts/BirdScript.cs
+++ Assets/Albin/Scripts/BirdScript.cs
@@ -6,7 +6,9 @@ public class BirdScript : MonoBehaviour
 {
 
     public GameObject birdObject;
-
+    private Vector3 direction;
+    public float gravity = -9.8f;
+    public float strength = 5f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,12 +19,16 @@ public class BirdScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            direction = Vector3.up * strength;
+            Debug.Log("helloworld");
+        }
+
+        direction.y += gravity * Time.deltaTime;
+        transform.position += direction * Time.deltaTime;
     }
 
 
-    private void OnMouseDown()
-    {
-        
-    }
+    
 }
