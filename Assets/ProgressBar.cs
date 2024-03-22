b94294ebd86af788c6696419a5aacf4f30b3ea16
Prilen <45573427+blueone69@users.noreply.github.com>
2024-03-22 16:35:51 +0100

    UI and script done

--- Assets/ProgressBar.cs
+++ Assets/ProgressBar.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
+// Video Tutorial https://www.youtube.com/watch?v=J1ng1zA3-Pk
+
+[ExecuteInEditMode()]
 public class ProgressBar : MonoBehaviour
 {
+    public int minimum;
     public int maximum;
     public int current;
+    public Image mask;
+
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +24,16 @@ public class ProgressBar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GetCurrentFill ();
     }
+
+
+    void GetCurrentFill ()
+    {
+        float currentOffset = current - minimum;
+        float maximumOffset = maximum - minimum;
+        float fillAmount = currentOffset / maximumOffset;
+        mask.fillAmount = fillAmount;
+    }
+
 }
