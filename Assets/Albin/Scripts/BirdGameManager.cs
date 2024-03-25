cbdd6d31d634b38189ec8139b58fded9b852e666
ChikinNoodleSupp <73000181+ChikinNoodleSupp@users.noreply.github.com>
2024-03-25 13:54:48 +0100

    I AM GOD

--- Assets/Albin/Scripts/BirdGameManager.cs
+++ Assets/Albin/Scripts/BirdGameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class BirdGameManager : MonoBehaviour
 {
@@ -11,6 +12,8 @@ public class BirdGameManager : MonoBehaviour
     public Spawner spawner;
     public bool isAlive = true;
     public BirdScript birdScript;
+    public bool DestroyWall = false;
+    public TextMeshProUGUI scoreUI;
 
     // Start is called before the first frame update
     void Awake()
@@ -22,6 +25,7 @@ public class BirdGameManager : MonoBehaviour
             manager = this;
            
         }
+        scoreUI.SetText("0");
     }
 
     // Update is called once per frame
@@ -32,24 +36,27 @@ public class BirdGameManager : MonoBehaviour
             Restart();
 
         }
+        
     }
 
     public void IncrementScore()
     {
         score++;
-        Debug.Log("added score");
+        scoreUI.SetText(score.ToString());
     }
 
     public void Restart()
     {
-        
+
         score = 0;
+        scoreUI.SetText(score.ToString());
         birdScript.ResetPosition();
         isAlive = true;
         birdScript.GravityStart = true;
         //birdScript.birdRigid.velocity = new Vector3(0, 0 ,0);
         birdScript.BirdJump();
         ToggleSpawner();
+        DestroyWall = false;
 
 
     }
@@ -65,5 +72,6 @@ public class BirdGameManager : MonoBehaviour
         if (!isAlive) return;
         ToggleSpawner();
         birdScript.ToggleGravity();
+        DestroyWall = true;
     }
 }
